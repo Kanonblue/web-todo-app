e9ebc027ee29620a54105f73a9dcafabddf0e9c3
Kanonblue <marutsukikanon@gmail.com>
2025-12-25 16:02:59 +0900

    UI調整：タスク一覧の配色・文字サイズを改善To Do アプリの完成

--- app/routes/web.php
+++ app/routes/web.php
@@ -24,3 +24,10 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//ここから追加
+Route::resource('todo', 'App\Http\Controllers\TodoController');
+Route::put('todo/done/{todo}',[App\Http\Controllers\TodoController::class, 'done'])->name('todo.done');
+Route::put('todo/undone/{todo}', [App\Http\Controllers\TodoController::class, 'undone'])->name('todo.undone');
+//ここまで追加
+
